6cf370a85e138bc1b90b977db9a34365931c351a
lucas-amberg <102396588+lucas-amberg@users.noreply.github.com>
2025-03-12 11:00:41 -0700

    Fix errors

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -132,7 +132,7 @@ export default function LoginPage() {
 
                 <CardFooter className="flex justify-center">
                     <p className="text-sm text-gray-600">
-                        Don't have an account?{" "}
+                        Don&apos;t have an account?{" "}
                         <Link
                             href="/signup"
                             className="font-medium text-blue-600 hover:text-blue-500">
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,10 @@
-import { clsx, type ClassValue } from "clsx";
+import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
+}
+
+export function isObject(item: unknown): item is Record<string, unknown> {
+    return item !== null && typeof item === "object" && !Array.isArray(item);
 }
